b1a231d5c72fa30149fb3e5c144f55cad95be8d3
jameskim3 <ipofri2@gmail.com>
2018-12-03 00:13:19 +0900

    181202

--- 181126.Streaming/sol_181126.cpp
+++ 181126.Streaming/sol_181126.cpp
@@ -3,9 +3,10 @@
 #define MAxServer (10)
 
 #define MAX_SIZE 10000
+int LE, NU, CL, AXIS[MAxServer];
 
 typedef struct {
-	int id, val;
+	int id, val, pos;
 }PQ;
 PQ heap1[10][MAX_SIZE];
 PQ heap2[10][MAX_SIZE];
@@ -14,9 +15,11 @@ int heapSize1[10];
 int heapSize2[10];
 
 typedef struct{
-	int dist[10];
-	int order[10];
+	int se_dist[10];
+	int se_order[10];
 	int se;
+	int id;
+	int val;
 }ND;
 ND nd[10000];
 int rp;
@@ -31,71 +34,6 @@ int compare(PQ a, PQ b){
 	if (a.val < b.val || (a.val == b.val && a.id > b.id))
 		return 1;
 	return -1;
-}
-int compare2(PQ a, PQ b){
-	if (a.val < b.val || (a.val == b.val && a.id < b.id))
-		return 1;
-	return -1;
-}
-int heapPush_remain(int se, int id, int value)
-{
-	if (heapSize1[se] + 1 > MAX_SIZE)
-		return 0;
-	if (heapSize2[se] + 1 > MAX_SIZE)
-		return 0;
-	int size1 = heapSize2[se];
-	heap2[se][size1].id = id;
-	heap2[se][size1].val = value;
-
-	int current = size1;
-	while (current > 0 && compare2(heap2[se][current], heap2[se][(current - 1) / 2]) == 1)
-	{
-		PQ temp = heap2[se][(current - 1) / 2];
-		heap2[se][(current - 1) / 2] = heap2[se][current];
-		heap2[se][current] = temp;
-		current = (current - 1) / 2;
-	}
-
-	heapSize2[se] = heapSize2[se] + 1;
-	return 1;
-}
-int heapPush(int se, int id, int value, int visited)
-{
-	if (heapSize1[se] + 1 > MAX_SIZE)
-		return 0;
-	if (heapSize2[se] + 1 > MAX_SIZE)
-		return 0;
-	int size1 = heapSize1[se];
-	heap1[se][size1].id = id;
-	heap1[se][size1].val = -value;
-
-	int current = size1;
-	while (current > 0 && compare(heap1[se][current], heap1[se][(current - 1) / 2]) == 1)
-	{
-		PQ temp = heap1[se][(current - 1) / 2];
-		heap1[se][(current - 1) / 2] = heap1[se][current];
-		heap1[se][current] = temp;
-		current = (current - 1) / 2;
-	}
-	nd[id].se = se;
-
-	heapSize1[se] = heapSize1[se] + 1;
-	if (heapSize1[se] > CL){
-		PQ poped;
-		heapPop(se, &poped);
-		heapPush_remain(se, poped.id, -poped.val);
-		int next_pos = 0;
-		ND* poped_id = &nd[poped.id];
-		for (int i = 0; i < 10; i++){
-			int val = visited >> poped_id->order[i];
-			if (val & 1)continue;
-			visited += 1 << poped_id->order[i];
-			int n = poped_id->order[i];
-			heapPush(n, poped.id, poped_id->dist[n], visited);
-			break;
-		}
-	}
-	return 1;
 }
 int heapPop(int se, PQ *value)
 {
@@ -135,41 +73,178 @@ int heapPop(int se, PQ *value)
 	}
 	return 1;
 }
-int LE, NU, CL, AXIS[MAxServer];
+int heapPop_remain(int se, PQ *value){
+	if (heapSize2[se] == 0)
+		return 0;
+
+	*value = heap2[se][0];
+	heapSize2[se] = heapSize2[se] - 1;
+
+	int size1 = heapSize2[se];
+	heap2[se][0] = heap2[se][size1];
+
+	int current = 0;
+	while (current * 2 + 1 < heapSize2[se])
+	{
+		int child;
+		if (current * 2 + 2 == heapSize2[se])
+		{
+			child = current * 2 + 1;
+		}
+		else
+		{
+			child = compare(heap2[se][current * 2 + 1], heap2[se][current * 2 + 2]) == 1 ? current * 2 + 2 : current * 2 + 1;
+		}
+
+		if (compare(heap2[se][current], heap2[se][child]) == -1)
+		{
+			break;
+		}
+
+		PQ temp = heap2[se][current];
+		heap2[se][current] = heap2[se][child];
+		heap2[se][child] = temp;
+		current = child;
+	}
+	return 1;
+}
+int heapPush_remain(int se, int id, int value, int pos)
+{
+	if (heapSize1[se] + 1 > MAX_SIZE)
+		return 0;
+	if (heapSize2[se] + 1 > MAX_SIZE)
+		return 0;
+	int size1 = heapSize2[se];
+	heap2[se][size1].id = id;
+	heap2[se][size1].val = value;
+	heap2[se][size1].pos = pos;
+
+	int current = size1;
+	while (current > 0 && compare(heap2[se][current], heap2[se][(current - 1) / 2]) == -1)
+	{
+		PQ temp = heap2[se][(current - 1) / 2];
+		heap2[se][(current - 1) / 2] = heap2[se][current];
+		heap2[se][current] = temp;
+		current = (current - 1) / 2;
+	}
+
+	heapSize2[se] = heapSize2[se] + 1;
+	return 1;
+}
+int heapPush(int se, int id, int value, int pos, int visited)
+{
+	if (heapSize1[se] + 1 > MAX_SIZE)
+		return 0;
+	if (heapSize2[se] + 1 > MAX_SIZE)
+		return 0;
+	int size1 = heapSize1[se];
+	heap1[se][size1].id = id;
+	heap1[se][size1].val = value;
+	heap1[se][size1].pos = pos;
+
+	int current = size1;
+	while (current > 0 && compare(heap1[se][current], heap1[se][(current - 1) / 2]) == 1)
+	{
+		PQ temp = heap1[se][(current - 1) / 2];
+		heap1[se][(current - 1) / 2] = heap1[se][current];
+		heap1[se][current] = temp;
+		current = (current - 1) / 2;
+	}
+	nd[pos].se = se;
+
+	heapSize1[se] = heapSize1[se] + 1;
+	if (heapSize1[se] > CL && visited != -1){
+		PQ poped;
+		heapPop(se, &poped);
+		heapPush_remain(se, poped.id, poped.val, poped.pos);
+		ND* tmp = &nd[poped.pos];
+		for (int i = 0; i < 10; i++){
+			int val = visited >> tmp->se_order[i];
+			if (val & 1)continue;
+			visited += 1 << tmp->se_order[i];
+			int new_se = tmp->se_order[i];
+			heapPush(new_se, tmp->id, tmp->val, poped.pos, visited);
+			break;
+		}
+	}
+	return 1;
+}
 void init(int L, int N, int C, int axis[MAxServer])
 {
 	LE = L, NU = N, CL = C;
 	for (int i = 0; i < NU; i++){
 		AXIS[i] = axis[i];
 	}
+	for (int i = 0; i < rp; i++){
+		nd[i].id = 0, nd[i].se = 0;
+	}
 	rp = 0;
 }
 int abs(int a){ return a>0 ? a : -a; }
 int add_user(int uid, int axis){
 	ND* tmp = &nd[rp++];
-	int dist[10], order[10];
+	int se_dist[10], se_order[10];
 	for (int i = 0; i < NU; i++){
 		int d1 = abs(AXIS[i] - axis);
 		int d2 = axis + (LE - AXIS[i]);
-		tmp->dist[i] = d1 > d2 ? d2 : d1;
-		tmp->order[i] = i;
+		tmp->se_dist[i] = d1 > d2 ? d2 : d1;
+		tmp->se_order[i] = i;
 	}
 	for (int i = 0; i < NU; i++){
-		for (int j = i+1; j < NU; j++){
-			if (tmp->dist[i] < tmp->dist[j]){
-				int t = tmp->dist[i];
-				tmp->dist[i] = tmp->dist[j]; tmp->dist[j] = t;
-				t = tmp->order[i];
-				tmp->order[i] = tmp->order[j]; tmp->order[j] = t;
+		for (int j = i + 1; j < NU; j++){
+			if (tmp->se_dist[i] > tmp->se_dist[j]){
+				int t = tmp->se_dist[i];
+				tmp->se_dist[i] = tmp->se_dist[j]; tmp->se_dist[j] = t;
+				t = tmp->se_order[i];
+				tmp->se_order[i] = tmp->se_order[j]; tmp->se_order[j] = t;
 			}
 		}
 	}
+	tmp->id = uid;
+	tmp->val = tmp->se_dist[NU - 1];
 	int visited = 0;
-	visited += 1 << tmp->order[0];
-	heapPush(tmp->order[0], rp - 1, tmp->dist[0],visited);
+	visited += 1 << tmp->se_order[0];
+	heapPush(tmp->se_order[0], tmp->id, tmp->val, rp - 1, visited);
+	return nd[rp - 1].se;
 }
 int remove_user(int uid);
-int get_users(int sid);
+void resizeServer(int se){
+	PQ pq;
+	int ret = heapPop_remain(se, &pq);
+	if (ret == 0)return;
+	remove_user(pq.id);
+
+	int visited = 0;
+	visited += 1 << se;
+	heapPush(se, pq.id, pq.val, pq.pos, visited - 1);
+}
+int remove_user(int uid){
+	int pos = 0;
+	while (nd[pos].id != uid)pos++;
+
+	int se = nd[pos].se;
+	PQ tmp;
+	PQ store[10000];
+	int cnt = 0;
+	int hsize = heapSize1[se];
+	for (int j = 0; j < hsize; j++){
+		heapPop(se, &tmp);
+		if (tmp.id == uid)break;
+		store[cnt].id = tmp.id;
+		store[cnt].val = tmp.val;
+		store[cnt].pos = tmp.pos;
+		cnt++;
+	}
+	while (cnt > 0){
+		heapPush(se, store[cnt-1].id, store[cnt - 1].val, store[cnt - 1].pos, -1);
+		cnt--;
+	}
+	resizeServer(se);
+	return se;
+}
+int get_users(int sid){
+	return heapSize1[sid];
+}
 
 /*
 1
